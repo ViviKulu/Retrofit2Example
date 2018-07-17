1a8925f8bc28a94596e4ef7a99bf8c95bc073d28
ViviKulu <babimaji1111@gmail.com>
2018-07-17 00:39:22 -0400

    ReactiveX set

--- app/src/main/java/com/example/vivianbabiryekulumba/retrofit2example/MainActivity.java
+++ app/src/main/java/com/example/vivianbabiryekulumba/retrofit2example/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.vivianbabiryekulumba.retrofit2example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.example.vivianbabiryekulumba.retrofit2example.view.AppAdapter;
+import com.example.vivianbabiryekulumba.retrofit2example.pojos.Data;
+import java.util.List;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity.class";
+    private List<Data> dataList;
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+
+        AppAdapter appAdapter = new AppAdapter(dataList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(appAdapter);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://rickandmortyapi.com/api/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+
     }
 }
